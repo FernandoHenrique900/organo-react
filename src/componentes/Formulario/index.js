4f8744fefdbd996eee3edcbd4ed3768d70ee671c
Fernando Henrique <fernando.henrique14@outlook.com>
2026-04-24 07:59:36 -0300

    feat(Formulario): adicionar componente ListaSuspensa para seleção de time
    Integrar um novo componente de dropdown que permite ao usuário selecionar um time a partir de uma lista predefinida.
    Isso substitui a entrada manual de texto para o campo “Time”, melhorando a experiência do usuário e garantindo maior consistência dos dados.

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,21 @@
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = () => {
+
+    const times =[
+        'Programação','Front-End','Data Science','Devops','UX-Design','Mobile','Inovação-Gestão'
+    ]
+
     return (
         <section className= "formulario">
             <form>
                 <h2>Preencha os dados pra criar o card do colaborador</h2>
                 <CampoTexto label="Nome" placeholder="Digite seu nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da sua imagem" />        
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da sua imagem" />
+                <ListaSuspensa label="Time" itens={times}/>       
             </form>
         </section>
     )
